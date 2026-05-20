a3e9812d194d15fb5e33e2452d9830469499aab3
markjocas-tmh <Mark.Jocas@mobilityhouse.com>
2026-05-20 08:58:57 +0200

    Decrease button click timeout

--- examples/companion_radio/ui-orig/Button.h
+++ examples/companion_radio/ui-orig/Button.h
@@ -5,7 +5,7 @@
 
 // Button timing configuration
 #define BUTTON_DEBOUNCE_TIME_MS    50      // Debounce time in ms
-#define BUTTON_CLICK_TIMEOUT_MS    500     // Max time between clicks for multi-click
+#define BUTTON_CLICK_TIMEOUT_MS    300     // Max time between clicks for multi-click
 #define BUTTON_LONG_PRESS_TIME_MS  3000    // Time to trigger long press (3 seconds)
 #define BUTTON_READ_INTERVAL_MS    10      // How often to read the button
 
